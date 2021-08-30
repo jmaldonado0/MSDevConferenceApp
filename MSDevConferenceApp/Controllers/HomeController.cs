0f40c38af5dba4effe85b1b58a3a5681936cef3e
jmaldonado0 <55056777+jmaldonado0@users.noreply.github.com>
2021-08-30 07:40:04 -0400

    Initial commit

---
+++ MSDevConferenceApp/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Web.Services3.Referral;
+using MSDevConferenceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSDevConferenceApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        } 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public ViewResult RegisterForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RegisterForm(WebinarInvites registrationResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(registrationResponse);
+                return View("ThankYou", registrationResponse);
+            }
+            else
+            {
+                return View();
+            }
+
+
+
+
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillJoin == true));
+        }
+        }
+    }
